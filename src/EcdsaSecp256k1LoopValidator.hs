0be164e764228e2b686973d5a5f6234a1bc0f53c
James Browning <james.browning@iohk.io>
2022-11-09 14:16:41 +0000

    Update dependencies and add SECP256k1 loop scripts

---
+++ src/EcdsaSecp256k1LoopValidator.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module EcdsaSecp256k1LoopValidator (writeSerialisedScript) where
+
+import           Cardano.Api           (PlutusScript, PlutusScriptV2,
+                                        writeFileTextEnvelope)
+import           Cardano.Api.Shelley   (PlutusScript (..))
+import           Codec.Serialise       (serialise)
+import qualified Data.ByteString.Lazy  as LBS
+import qualified Data.ByteString.Short as SBS
+import           Data.Functor          (void)
+import qualified Plutus.V2.Ledger.Api  as PlutusV2
+import qualified PlutusTx
+import qualified PlutusTx.Builtins     as BI
+import           PlutusTx.Prelude      as P hiding (Semigroup (..), unless, (.))
+import           Prelude               (IO, (.))
+
+{-# INLINEABLE mkValidator #-}
+mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkValidator _datum red _txContext =
+  case PlutusV2.fromBuiltinData red of
+    Nothing -> P.traceError "Trace error: Invalid redeemer"
+    Just (n, vkey, msg, sig) ->
+      if n < (0 :: Integer)
+      then traceError "redeemer is < 0"
+      else loop n vkey msg sig
+  where
+    loop i v m s
+      | i == 0 = ()
+      | BI.verifyEcdsaSecp256k1Signature v m s = loop (pred i) v m s
+      | otherwise = P.traceError "Trace error: ECDSA validation failed"
+
+validator :: PlutusV2.Validator
+validator = PlutusV2.mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+
+script :: PlutusV2.Script
+script = PlutusV2.unValidatorScript validator
+
+loopScriptShortBs :: SBS.ShortByteString
+loopScriptShortBs = SBS.toShort . LBS.toStrict $ serialise script
+
+loopScript :: PlutusScript PlutusScriptV2
+loopScript = PlutusScriptSerialised loopScriptShortBs
+
+writeSerialisedScript :: IO ()
+writeSerialisedScript = void $ writeFileTextEnvelope "ecdsa-secp256k-loop.plutus" Nothing loopScript
